8ca9cf0f203e24544bac8f102c6f6d88a04b8893
elaineawang <elaine.wang@sonder.com>
2018-11-16 23:11:46 +0800

    color on scroll

--- scroll.js
+++ scroll.js
@@ -1,7 +1,7 @@
 $(window).scroll(function() {    
     var scroll = $(window).scrollTop();
 
-    if (scroll >= 200) {
+    if (scroll >= 150) {
         $("body").addClass("color");
     } else {
         $("body").removeClass("color");
