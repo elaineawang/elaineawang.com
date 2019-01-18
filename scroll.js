0935bfe7319eb45cdcf656aa0ceae9f2650cb7fa
elaineawang <elaine.wang@sonder.com>
2019-01-17 21:39:27 -0800

    bouce

--- scroll.js
+++
@@ -1,9 +0,0 @@
-$(window).scroll(function() {    
-    var scroll = $(window).scrollTop();
-
-    if (scroll >= 150) {
-        $("body").addClass("color");
-    } else {
-        $("body").removeClass("color");
-    }
-});
